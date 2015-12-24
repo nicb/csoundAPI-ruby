ab153f8f177437394077559a8e443e1e148a1a7d
Nicola Bernardini <nicb@sme-ccppd.org>
2015-12-24 04:39:46 +0100

    added quiet function to speccing; added travis deployment

--- spec/csound_lib_spec.rb
+++ spec/csound_lib_spec.rb
@@ -1,20 +1,26 @@
 require 'spec_helper'
 
+def quiet
+  #
+  # we want to shut off STDERR to avoid seeing csound's output
+  #
+  stderr_backup = $stderr.dup
+  $stderr.reopen('/dev/null', 'w')
+
+  yield
+
+  #
+  # resume normal STDERR
+  #
+  $stderr = stderr_backup.dup
+end
+
 describe CsoundAPIRuby::CsoundLib do
 
   it 'runs the basic csound API interface' do
-    #
-    # we want to shut off STDERR to avoid seeing csound's output
-    #
-    stderr_backup = $stderr.dup
-    $stderr.reopen('/dev/null', 'w')
-
-    expect((p = CsoundAPIRuby::CsoundLib::csoundCreate(0)).class).to be(FFI::Pointer)
-
-    #
-    # resume normal STDERR
-    #
-    $stderr = stderr_backup.dup
+    quiet do
+      expect((p = CsoundAPIRuby::CsoundLib::csoundCreate(0)).class).to be(FFI::Pointer)
+    end
   end
 
 end
