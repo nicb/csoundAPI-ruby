6884652e6394838f2560dec589d96164f1f8999b
Nicola Bernardini <nicb@sme-ccppd.org>
2015-12-28 22:23:11 +0100

    blind attempt to make osx work

--- lib/csoundAPI_ruby/lib/functions/header.rb
+++ lib/csoundAPI_ruby/lib/functions/header.rb
@@ -8,7 +8,13 @@ module CsoundAPIRuby
 
       extend FFI::Library
 
-      CSOUND_LIBRARY_PATH = File.expand_path(File.join(['..'] * 5, 'ext', 'cs6make', 'libcsnd6.so'), __FILE__)
+      LIBRARY_SUFFIX = case RUBY_PLATFORM
+                       when /linux/i then '.so'
+                       when /darwin/i then '.dylib'
+                       when /windows/i then '.dll'
+                       else raise StandardError, "Unknown platform #{RUBY_PLATFORM}"
+                       end
+      CSOUND_LIBRARY_PATH = File.expand_path(File.join(['..'] * 5, 'ext', 'cs6make', "libcsnd6#{LIBRARY_SUFFIX}"), __FILE__)
       ffi_lib CSOUND_LIBRARY_PATH
 
     end
